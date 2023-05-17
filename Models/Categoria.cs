d052d438e380037a1f5ee33ec6281ac070b20a9d
Victorcalp <victorcalp@outlook.com.br>
2023-05-16 23:24:25 -0300

    Definido os Data Annotations de Categoria e Lanche

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,11 +1,23 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
 
 namespace LanchesMac.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Deve se informar o nome da categoria")]
+        [StringLength(100)]
+        [Display(Name = "Nome")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição")]
+        [StringLength (200, ErrorMessage = "Deve ter no maximo 200 caracteres")]
+        [Display(Name = "Descrição")]
         public string? Descricao { get; set; }
 
         public List<Lanche>? Lanches { get; set; }
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,14 +1,50 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+
+        //ErrorMessage - consegue alterar o erro que aparece para o usuario
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} de ter no minimo {1} e no maximo {2} caracteres")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "A descriação do lanche deve ser informada")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = "Descriação deve ter no minimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no maximo {1} caracteres")]
         public string? DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição deve ser informada")]
+        [Display(Name = "Descrição Detalhada")]
+        [StringLength(200, MinimumLength = 50, ErrorMessage = "Informe a descrição detalhada")]
+        public string? DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o Preço")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")] // definiu como decimal, com 10 digitos e 2 casas decimais
+        [Range(1, 999.999, ErrorMessage = "O Preço deve estar entre 1 e 999,999")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho da imagem normal")]
+        [StringLength(200)]
         public string? ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho da imagem miniatura")]
+        [StringLength(200)]
         public string? ImagemThumbnaiUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
 
